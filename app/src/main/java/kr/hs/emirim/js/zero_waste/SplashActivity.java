925e6894e73592ddc63e3735f5477cf05a05821e
dayeon <dayeon2399@naver.com>
2021-06-08 19:38:27 +0900

    splash view

---
+++ app/src/main/java/kr/hs/emirim/js/zero_waste/SplashActivity.java
@@ -0,0 +1,18 @@
+package kr.hs.emirim.js.zero_waste;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
