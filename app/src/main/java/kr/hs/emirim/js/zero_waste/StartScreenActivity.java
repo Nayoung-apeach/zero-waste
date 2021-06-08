f986fc4a27732557d559e6d81a36a53500728c97
dayeon <dayeon2399@naver.com>
2021-06-09 03:55:08 +0900

    start screen 화면전환

--- app/src/main/java/kr/hs/emirim/js/zero_waste/StartScreenActivity.java
+++ app/src/main/java/kr/hs/emirim/js/zero_waste/StartScreenActivity.java
@@ -1,6 +1,9 @@
 package kr.hs.emirim.js.zero_waste;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,25 @@ public class StartScreenActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startscreen);
+
+        Button login_button = findViewById(R.id.start_screen_button_login);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Button join_button = findViewById(R.id.start_screen_button_join);
+        join_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartScreenActivity.this, JoinActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
