a90815180ff11c3ffc350ae9a5eaaa3c11732a66
dayeon <dayeon2399@naver.com>
2021-06-14 06:53:01 +0900

    회원가입 성공

---
+++ app/src/main/java/kr/hs/emirim/js/zero_waste/LoginActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.js.zero_waste;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+// dayeon-choi
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
