5ee24c198467ec7de4cc9614152c69df461079df
dayeon <dayeon2399@naver.com>
2021-06-15 05:01:00 +0900

    로그인 화면 구현

--- app/src/main/java/kr/hs/emirim/js/zero_waste/LoginActivity.java
+++ app/src/main/java/kr/hs/emirim/js/zero_waste/LoginActivity.java
@@ -1,6 +1,8 @@
 package kr.hs.emirim.js.zero_waste;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 // dayeon-choi
 
 public class LoginActivity extends AppCompatActivity {
+//    Button mLoginBtn;
+//    TextView mResigettxt;
+//    EditText mEmailText, mPasswordText;
+
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
