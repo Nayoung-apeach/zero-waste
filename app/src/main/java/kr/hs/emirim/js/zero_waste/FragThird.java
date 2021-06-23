ffbae53afdfd67164422ad1dce0e9b2ac544d9a4
seobin Park <s2019w28@e-mirim.hs.kr>
2021-06-24 07:10:24 +0900

    Add Mission

---
+++ app/src/main/java/kr/hs/emirim/js/zero_waste/FragThird.java
@@ -0,0 +1,56 @@
+package kr.hs.emirim.js.zero_waste;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+
+public class FragThird extends Fragment {
+    Dialog mission_done;
+    ImageView earth;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.third_mission, container, false);
+        earth = rootView.findViewById(R.id.imageView);
+        mission_done =new Dialog(getActivity());
+        mission_done.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        mission_done.setContentView(R.layout.activity_mission_dialog);
+        earth.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+                mission_done.show();
+                mission_done.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                Button noBtn = mission_done.findViewById(R.id.noBtn);
+                Button yesBtn = mission_done.findViewById(R.id.yesBtn);
+                yesBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mission_done.dismiss();
+                    }
+                });
+                noBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // 원하는 기능 구현
+                        mission_done.dismiss();
+                    }
+                });
+            }
+        });
+
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/kr/hs/emirim/js/zero_waste/level.java
@@ -0,0 +1,20 @@
+package kr.hs.emirim.js.zero_waste;
+
+public class level {
+    int level;
+
+    public level(){}
+
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public level(int level){
+        this.level = level;
+    }
+}
